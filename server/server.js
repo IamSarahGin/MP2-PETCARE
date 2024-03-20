21a9cb928ad8d196bf7c25b20245fd140198266f
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-03-20 18:24:54 +0800

    new

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://mp-2-pet-care.vercel.app"],
     method: ["POST", "GET"],
     credentials: true
 }));
