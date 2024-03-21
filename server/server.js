77b3f2ad8a8f483b97efad56b3f92640b7f95148
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-03-21 20:32:02 +0800

    new

--- server/server.js
+++ server/server.js
@@ -132,7 +132,7 @@ app.post('/login', (req, res) => {
                   // Generate token
                   const token = jwt.sign({ userId, firstName, email, role }, "jwt-secret-key", { expiresIn: '1d' });
                   // Set token in cookie
-                  res.cookie("token", token, { httpOnly: true, sameSite: 'none', maxAge: 24 * 60 * 60 * 1000 }); // 1 day expiry
+                  res.cookie("token", token, { httpOnly: true, sameSite: 'strict', maxAge: 24 * 60 * 60 * 1000 }); // 1 day expiry
                   return res.json({ Status: "Success", role: role }); 
               } else {
                   return res.json({ Error: "Password not matched" });
