321d1859e2e012070b21d2b5353f6c8b79136046
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-03-21 17:42:11 +0800

    new

--- server/server.js
+++ server/server.js
@@ -198,7 +198,7 @@ app.get('/bookings', verifyUser, (req, res) => {
   SELECT booking.*, users.email AS userEmail
   FROM booking 
   INNER JOIN users ON booking.userId = users.userId 
-  WHERE boking.userId = ?`;
+  WHERE booking.userId = ?`;
   db.query(getBookingsQuery, [userId], (err, results) => {
       if (err) {
           console.error('Error fetching bookings:', err);
@@ -326,7 +326,7 @@ app.get('/api/bookings/approved', verifyUser, verifyAdmin, (req, res) => {
 
 // API endpoint to fetch rejected bookings
 app.get('/api/bookings/rejected', verifyUser, verifyAdmin, (req, res) => {
-  const query = 'SELECT Booking.*, users.firstName, users.lastName, users.email FROM booking INNER JOIN users ON Booking.userId = users.userId WHERE Booking.status = ?';
+  const query = 'SELECT booking.*, users.firstName, users.lastName, users.email FROM booking INNER JOIN users ON booking.userId = users.userId WHERE booking.status = ?';
   db.query(query, ['rejected'], (err, results) => {
       if (err) {
           console.error('Error fetching rejected bookings:', err);
