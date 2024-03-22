3cfabebd8c626380d0370adc4faf4ba7f075bc7c
Sarah Gallardo <gallardosarahgin2023@gmail.com>
2024-03-22 15:09:45 +0800

    new

--- frontend/petcare/vite.config.js
+++ frontend/petcare/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base:"/MP2-PetCare/frontend/petcare",
   server:{
     port:3000,
   },
